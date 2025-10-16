3538678c709c09549172d0548cd06e9897ec2bfb
장선규 <dsng3419@gmail.com>
2025-10-16 12:58:51 +0900

    1

--- app.py
+++ app.py
@@ -76,4 +76,4 @@ def save_study_time():
     return jsonify({"status": "success", "data": data})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=2424)
